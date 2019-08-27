e3816f06e5c98d88eeda09e29724a25065d13b91
Emre YAZICI <IEmreYazici@suitsupply.com>
2019-08-27 10:50:42 +0200

    projections function updates

--- src/query-side/OrderContext.Projections/ProjectionHandler.cs
+++ src/query-side/OrderContext.Projections/ProjectionHandler.cs
@@ -1,25 +1,100 @@
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using OrderContext.Domain.Messages.Orders;
+using OrderContextQueryClient;
+using OrderCancelledEvent = OrderContext.Domain.Messages.Orders.OrderCancelledEvent;
+using OrderPaidEvent = OrderContext.Domain.Messages.Orders.OrderPaidEvent;
+using OrderShippedEvent = OrderContext.Domain.Messages.Orders.OrderShippedEvent;
+using OrderStartedEvent = OrderContext.Domain.Messages.Orders.OrderStartedEvent;
 
 namespace OrderContext.Projections
 {
     public static class ProjectionHandler
     {
         [FunctionName("ProjectionHandler")]
-        public static void Run([CosmosDBTrigger(
-            databaseName: "databaseName",
-            collectionName: "collectionName",
-            ConnectionStringSetting = "",
-            LeaseCollectionName = "leases-projection")]IReadOnlyList<Document> input, ILogger log)
+        public static async Task Run([CosmosDBTrigger(
+            databaseName: "OrderContextES",
+            collectionName: "Events",
+            ConnectionStringSetting = "ConStr",
+            LeaseCollectionName = "leases-projection",
+            StartFromBeginning = true,
+            CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> input, ILogger log)
         {
-            if (input != null && input.Count > 0)
+            foreach (var @event in input)
             {
-                log.LogInformation("Documents modified " + input.Count);
-                log.LogInformation("First document Id " + input[0].Id);
+                switch (CastEventToDynamic(@event))
+                {
+                    case OrderStartedEvent e:
+                        await SendOrderStarted(e);
+                        break;
+                    case OrderPaidEvent e:
+                        await SendOrderPaid(e);
+                        break;
+                    case OrderShippedEvent e:
+                        await SendOrderShipped(e);
+                        break;
+                    case OrderCancelledEvent e:
+                        await SendOrderCancelled(e);
+                        break;
+                }
             }
+        }
+
+        private static async Task SendOrderStarted(OrderStartedEvent @event)
+        {
+            var client = new Client("http://localhost:5001", HttpClientFactory.Create());
+            await client.WhenAsync(new OrderContextQueryClient.OrderStartedEvent
+            {
+                BuyerId = @event.BuyerId,
+                City = @event.City,
+                OrderId = @event.OrderId,
+                Street = @event.Street
+            });
+
+        }
+
+        private static async Task SendOrderPaid(OrderPaidEvent @event)
+        {
+            var client = new Client("http://localhost:5001", HttpClientFactory.Create());
+            await client.WhenAsync(new OrderContextQueryClient.OrderPaidEvent
+            {
+                OrderId = @event.OrderId
+            }); 
+        }
+
+        private static async Task SendOrderShipped(OrderShippedEvent @event)
+        {
+            var client = new Client("http://localhost:5001", HttpClientFactory.Create());
+            await client.WhenAsync(new OrderContextQueryClient.OrderShippedEvent
+            {
+                OrderId = @event.OrderId
+            });
+        }
+        private static async Task SendOrderCancelled(OrderCancelledEvent @event)
+        {
+            var client = new Client("http://localhost:5001", HttpClientFactory.Create());
+            await client.WhenAsync(new OrderContextQueryClient.OrderCancelledEvent
+            {
+                OrderId = @event.OrderId
+            });
+        }
+        private static dynamic CastEventToDynamic(Document @event)
+        {
+            var eData = ((dynamic)@event).Data;
+            var eType = ((dynamic)@event).Type;
+
+            var type = Convert.ToString(eType);
+
+            var castedEvent = JsonConvert.DeserializeObject(eData, Type.GetType(type));
+
+            return castedEvent;
         }
     }
 }
