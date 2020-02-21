117c0777498c7258c2384b6510f11b9b353fe396
Emre YAZICI <IEmreYazici@suitsupply.com>
2020-02-21 14:55:35 +0100

    functionextensions added for mediatr httpreq handles

---
+++ es/src/command-side/OrderContext.Command.Function.Host/Extensions/FunctionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json; 
+using System.IO; 
+using System.Threading.Tasks;
+
+namespace OrderContext.Command.Function.Host
+{
+    public static class FunctionExtensions
+    {  
+        public static async Task<IActionResult> Send<TCommand>(this IMediator mediator,
+            HttpRequest httpRequest)
+        {
+            var requestBody = await new StreamReader(httpRequest.Body).ReadToEndAsync().ConfigureAwait(false);
+
+            var command = JsonConvert.DeserializeObject<TCommand>(requestBody);
+
+            var result = await mediator.Send(command).ConfigureAwait(false);
+
+            return new OkObjectResult(result); 
+        } 
+    }
+}
--- es/src/command-side/OrderContext.Command.Function.Host/Functions/OrderCommandFunctions.cs
+++ es/src/command-side/OrderContext.Command.Function.Host/Functions/OrderCommandFunctions.cs
@@ -5,32 +5,30 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using MediatR;
 using AzureFunctions.Extensions.Swashbuckle.Attribute;
 using static OrderContext.Domain.Messages.Orders.Commands;
 
 namespace OrderContext.Command.Function.Host
 {
-    public class OrderCommandFunctions : CommandFunctionBase
+    public class OrderCommandFunctions
     {
-        public OrderCommandFunctions(IMediator mediatr) : base(mediatr)
-        {
-        }
-
+        private readonly IMediator _mediatr;
+        public OrderCommandFunctions(IMediator mediatr) =>
+            _mediatr = mediatr;
+         
         [FunctionName("CreateOrderCommand")]
         public async Task<IActionResult> CreateOrder(
             [HttpTrigger(AuthorizationLevel.Function,  "post", Route = "v1/Orders")]
             [RequestBodyType(typeof(CreateOrderCommand), "create order")]HttpRequest req) =>
-             await Handle<CreateOrderCommand>(req);
+             await _mediatr.Send<CreateOrderCommand>(req);
 
 
         [FunctionName("PayOrderCommand")]
         public async Task<IActionResult> PayOrder(
             [HttpTrigger(AuthorizationLevel.Function,  "put", Route = "v1/Orders")]
-            [RequestBodyType(typeof(PayOrderCommand), "pay order")]HttpRequest req)=>
-            await Handle<PayOrderCommand>(req);
+            [RequestBodyType(typeof(PayOrderCommand), "pay order")]HttpRequest req) =>
+             await _mediatr.Send<PayOrderCommand>(req);
 
     }
 }
